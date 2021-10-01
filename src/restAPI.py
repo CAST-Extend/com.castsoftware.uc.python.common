f0fe1bcf3ffed3c75f6ba110a4cf8b15f2991e3b
Nevin Kaplan <n.kaplan@castsoftware.com>
2021-10-01 08:12:05 -0400

    housekeeping

--- src/restAPI.py
+++ src/restAPI.py
@@ -1,11 +1,15 @@
 import urllib.parse
-import requests
+
+from requests import get
+from requests import exceptions
+from requests.auth import HTTPBasicAuth 
+from requests import codes
 
 from logger import Logger
 from logging import INFO, error
 from pandas import DataFrame
 from time import perf_counter, ctime
-from requests.auth import HTTPBasicAuth 
+
 
 
 class RestCall(Logger):
@@ -17,6 +21,8 @@ class RestCall(Logger):
 
     def __init__(self, base_url, user=None, password=None, track_time=False,log_level=INFO):
         super().__init__("RestCall",log_level)
+        if base_url[-1]=='/': 
+            base_url=base_url[:-1]
         self._base_url = base_url
         self._track_time = track_time
         self._auth = HTTPBasicAuth(user, password)
@@ -26,11 +32,13 @@ class RestCall(Logger):
         start_tm = perf_counter()
 
         try:
+            if len(url) > 0 and url[0] != '/':
+                url=f'/{url}'
             u = urllib.parse.quote(f'{self._base_url}{url}',safe='/:&?=')
             if self._auth == None:
-                resp = requests.get(url= u, headers=headers)
+                resp = get(url= u, headers=headers)
             else:
-                resp = requests.get(url= u, auth = self._auth, headers=headers)
+                resp = get(url= u, auth = self._auth, headers=headers)
 
             # Save the duration, if enabled.
             if (self._track_time):
@@ -41,16 +49,21 @@ class RestCall(Logger):
                 #print(f'Request completed in {duration} ms')
                 self._time_tracker_df = self._time_tracker_df.append({'Application': 'ALL', 'URL': url, 'Start Time': start_dttm \
                                                             , 'End Time': end_dttm, 'Duration': duration}, ignore_index=True)
-            return resp.status_code, resp.json()
+            if resp.status_code == codes.ok:
+                return resp.status_code, resp.json()
+            else:
+                return resp.status_code,""
 
-        except requests.exceptions.Timeout:
+        except exceptions.ConnectionError:
+            self.error(f'Unable to connect to host {self._base_url}')
+        except exceptions.Timeout:
             #TODO Maybe set up for a retry, or continue in a retry loop
             self.error(f'Timeout while performing api request using: {url}')
-        except requests.exceptions.TooManyRedirects:
+        except exceptions.TooManyRedirects:
             #TODO Tell the user their URL was bad and try a different one
             self.error(f'TooManyRedirects while performing api request using: {url}')
-        except requests.exceptions.RequestException as e:
+        except exceptions.RequestException as e:
             # catastrophic error. bail.
             self.error(f'General Request exception while performing api request using: {url}')
 
-        return resp.status_code, "{}"
+        return 0, "{}"
