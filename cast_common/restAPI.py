7c2de5cd89e0897a7af4b585fd613ad5b1d3bc0e
Nevin Kaplan <n.kaplan@castsoftware.com>
2024-02-13 09:58:23 -0500

    aded token login capability

--- cast_common/restAPI.py
+++ cast_common/restAPI.py
@@ -4,6 +4,7 @@ from requests import get,exceptions,codes,Session
 from requests.auth import HTTPBasicAuth 
 from requests.adapters import HTTPAdapter, Retry
 from requests.exceptions import ConnectionError, ConnectTimeout
+import urllib3
 
 #from requests.packages.urllib3.util.retry import Retry
 
@@ -12,7 +13,7 @@ from cast_common.logger import Logger
 from pandas import DataFrame
 from time import perf_counter, ctime
 from base64 import b64decode
-
+from inspect import stack
 
 __author__ = "Nevin Kaplan"
 __copyright__ = "Copyright 2022, CAST Software"
@@ -25,34 +26,52 @@ class RestCall(Logger):
     _auth = None
     _time_tracker_df  = DataFrame()
     _track_time = True
+    _api_key = False
+    _session = {}
 
-    def __init__(self,*, base_url, user=None, password=None, basic_auth=None, track_time=False,log_level=INFO):
+    def __init__(self,*, base_url, user=None, password=None, basic_auth=None, api_key:bool=False, track_time=False,log_level=INFO):
         super().__init__(level=log_level)
         if base_url[-1]=='/': 
             base_url=base_url[:-1]
         self._base_url = base_url
-        self._track_time = track_time
 
-        self._session = Session()
-        self._max_retries = 5
+        if base_url not in RestCall._session:
 
-        self._adapter = HTTPAdapter(
-                max_retries = Retry(
-                    total = self._max_retries,
-                    backoff_factor = 1,
-                    status_forcelist = [408, 500, 502, 503, 504],
-                )
-        )
+            RestCall._session[base_url] = Session()
 
-        self._session.mount('http://', self._adapter)
-        self._session.mount('https://', self._adapter)
-        if basic_auth:
-            up = b64decode(bytes(basic_auth,encoding='utf8')+b'==')
-            (user,password)=up.decode().split(':')
-        self._auth = HTTPBasicAuth(user, password)
 
-        self._session.headers.update({'Accept': 'application/json'})
-        #self._session.headers.update({'Authorization': self._auth})
+            RestCall._session[base_url].verify = False
+            urllib3.disable_warnings()
+
+
+            max_retries = 5
+
+            self._adapter = HTTPAdapter(
+                    max_retries = Retry(
+                        total = max_retries,
+                        backoff_factor = 1,
+                        status_forcelist = [408, 500, 502, 503, 504],
+                    )
+            )
+
+            RestCall._session[base_url].mount('http://', self._adapter)
+            RestCall._session[base_url].mount('https://', self._adapter)
+            if basic_auth:
+                up = b64decode(bytes(basic_auth,encoding='utf8')+b'==')
+                (user,password)=up.decode().split(':')
+            self._auth = HTTPBasicAuth(user, password)
+
+            RestCall._session[base_url].headers.update({'Accept': 'application/json'})
+
+            self._api_key=api_key
+            if api_key:
+                RestCall._session[base_url].headers.update({'X-Api-Key': password})
+                RestCall._session[base_url].headers.update({'X-Api-User': user})
+                
+                #login to MRI rest api
+                (status,rslt) = self.get('login')
+                pass
+                
 
     def get(self, url = "",header=None):
         start_dttm = ctime()
@@ -62,36 +81,22 @@ class RestCall(Logger):
             if len(url) > 0 and url[0] != '/':
                 url=f'/{url}'
             u = urllib.parse.quote(f'{self._base_url}{url}',safe='/:&?=')
-            # if self._auth == None:
-            #     resp = get(url= u, headers={'Accept': 'application/json'})
-            # else:
-            #     resp = get(url= u, auth = self._auth, headers={'Accept': 'application/json'})
 
             if header is None:
                 header={'Accept': 'application/json'}
 
-#            resp = self._session.get(u, timeout = (5, 15),auth=self._auth,headers={'Accept': 'application/json'})
-            resp = self._session.get(u, timeout = (5, 15),auth=self._auth,headers=header)
+            resp = RestCall._session[self._base_url].get(u, timeout = (5, 15),auth=self._auth,headers=header)
             resp.raise_for_status()
 
-            # Save the duration, if enabled.
-            if (self._track_time):
-                end_tm = perf_counter()
-                end_dttm = ctime()
-                duration = end_tm - start_tm
-
-                #print(f'Request completed in {duration} ms')
-                self._time_tracker_df = self._time_tracker_df.append({'Application': 'ALL', 'URL': url, 'Start Time': start_dttm \
-                                                            , 'End Time': end_dttm, 'Duration': duration}, ignore_index=True)
             if resp.status_code == codes.ok:
                 return resp.status_code, resp.json()
             else:
                 return resp.status_code,""
 
-        except exceptions.ConnectionError:
-            self.error(f'Unable to connect to host {self._base_url}')
+        except exceptions.ConnectionError as ex:
+            self.error(f'Unable to connect to host {self._base_url}: {ex} ')
             exit()
-        except exceptions.Timeout:
+        except exceptions.Timeout as ex:
             #TODO Maybe set up for a retry, or continue in a retry loop
             self.error(f'Timeout while performing api request using: {url}')
         except exceptions.TooManyRedirects:
@@ -114,7 +119,7 @@ class RestCall(Logger):
                 url=f'/{url}'
             u = urllib.parse.quote(f'{self._base_url}{url}',safe='/:&?=')
 
-            resp = self._session.post(u,data=data,timeout = (5, 15),auth=self._auth,headers={'Accept': 'application/json'})
+            resp = RestCall._session[self._base_url].post(u,data=data,timeout = (5, 15),auth=self._auth,headers={'Accept': 'application/json'})
             resp.raise_for_status()
 
             if resp.status_code == codes.ok:
